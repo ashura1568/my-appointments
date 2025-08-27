9daff8bc994f7a2700481f9867511b380f2b78e3
andres felipe molina cruz <andresfmc100@gmail.com>
2025-08-26 23:16:22 -0500

    'Panelprincipal'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.panel')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="row">
+    <div class="col-md-12 mb-4">
+        <div class="card">
+            <div class="card-header">Panel de administracion</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
+                You are logged in!
             </div>
         </div>
     </div>
-</div>
+    </div>
+
 @endsection
