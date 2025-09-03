dd6acbdc3ffbf76fc385c6b9ad1c37a6533eb03f
andres felipe molina cruz <andresfmc100@gmail.com>
2025-09-02 22:46:04 -0500

    'Repositorio-GitHub'

--- app/Http/Controllers/Doctor/ScheduleController.php
+++ app/Http/Controllers/Doctor/ScheduleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Doctor;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\WorkDay;
+
 
 class ScheduleController extends Controller
 {
